eef44d1632cc7dbeba6d736a25713a121b3c749b
elizavetatrubaceva <lizatrue5@gmail.com>
2023-02-12 16:46:11 +0600

    Внесены правки согласно заданиям урока

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,9 @@
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser)
-
-
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
