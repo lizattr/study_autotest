c1951e19cefb08dd3b7827610c9b65c8eb727bc2
Lisa <lizatrue5@gmail.com>
2023-02-13 17:46:34 +0600

    Шаг 13, начало. Коммит необходим для переноса данных с одного десктопа на другой

--- test_product_page.py
+++ test_product_page.py
@@ -36,3 +36,21 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket_page()
     page.basket_is_empty()
     page.basket_is_empty_notification()
+
+
+class TestUserAddToBasketFromProductPage():
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = PageObject(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = PageObject(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        page.match_book_name()
+        page.match_price()
